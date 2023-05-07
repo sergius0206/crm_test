8ce6df0e0ae87d4b8aaaa7f71dcc56eebc01f476
sergius0206 <sergius0206@gmail.com>
2023-05-07 19:44:39 +0300

    login and logout

--- bodisite/bodi/views.py
+++ bodisite/bodi/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
-    return render(request, 'bodi/home.html', {})
+    """ Перевирка форми регистрациї """
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Все відно 'Раді Вас бачити на сайті'")
+            return redirect('home')
+        else:
+            messages.success(request, "Невірний логин чи пароль, спробуй ще.")
+            return redirect('home')
+    else:        
+        return render(request, 'bodi/home.html', {})
+
+# def login_user(request):
+#     pass    
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "Ви успічно вийщли з облікового запису")
+    return redirect('home')
